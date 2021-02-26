c19d227b2f1e69306f187c67ca6f09b7caac3408
Ubuntu <ubuntu@ip-172-31-30-106.ap-northeast-1.compute.internal>
2021-02-26 07:58:13 +0000

    kadai-tasklist-login

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_094251) do
+ActiveRecord::Schema.define(version: 2021_02_26_073825) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
-    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
     t.bigint "user_id"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
@@ -29,4 +29,5 @@ ActiveRecord::Schema.define(version: 2021_02_25_094251) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "tasks", "users"
 end
